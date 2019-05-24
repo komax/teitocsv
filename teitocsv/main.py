d0553650a14c6cfc01888c3ac1596fbaf4f33603
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-05-24 12:59:50 +0200

    Create first simple dataframe from tei files

--- teitocsv/main.py
+++ teitocsv/main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import argparse
-
+from pathlib import Path
 from multiprocessing.pool import Pool
 
+import pandas as pd
+
+from teireader import TEIFile
 
 def set_up_argparser():
     parser = argparse.ArgumentParser()
@@ -12,9 +15,27 @@ def set_up_argparser():
     return parser
 
 
+def all_teis(input_dir):
+    return sorted(Path(input_dir).glob('*.tei.xml'))
+
+
 def main():
     parser = set_up_argparser()
     args = parser.parse_args()
+    
+    result_csv = pd.DataFrame(columns=['ID', 'DOI','Title', 'Journal'])
+
+    for tei_file in all_teis(args.inputdir):
+        tei = TEIFile(tei_file)
+        result_csv = result_csv.append({
+            'ID':tei.basename(),
+            'DOI':tei.doi(),
+            'Title':tei.title(),
+            'Journal':tei.published_in()}, ignore_index=True)
+
+    print(result_csv)
+
+    args.outfile
 
     #pool = Pool()
 

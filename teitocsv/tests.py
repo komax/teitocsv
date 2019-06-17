e303a5ab62ad11ca684b44fd95e23d376aeb5d97
Maximilian Konzack <maximilian.konzack@gmail.com>
2019-06-17 18:41:58 +0200

    Unit tests for data source

--- teitocsv/tests.py
+++ teitocsv/tests.py
@@ -85,7 +85,35 @@ class DataSourceMatcherTest(unittest.TestCase):
         All sequence data will be made available through FigShare, http://dx.doi.org/10.22222/m9.figshare.1234567890.
         """
         data_source = self.matcher.data_source(text)
-        self.assertEqual(data_source, "FigShare")
+        self.assertEqual(data_source, "figshare")
+
+    def test_figshare_with_weird_spelling(self):
+        text = """
+        All sequence data will be made available through FIGShare, http://dx.doi.org/10.22222/m9.figshare.1234567890.
+        """
+        data_source = self.matcher.data_source(text)
+        self.assertEqual(data_source, "figshare")
+
+    def test_figshare_only_in_url(self):
+        text = """
+        All sequence data will be made available via http://dx.doi.org/10.22222/m9.figshare.1234567890.
+        """
+        data_source = self.matcher.data_source(text)
+        self.assertEqual(data_source, "figshare")
+
+    def test_qiita_in_text(self):
+        text = "All data available through QIITA."
+        data_source = self.matcher.data_source(text)
+        self.assertEqual(data_source, "qiita")
+
+    def test_mg_rast_in_text(self):
+        text = "All data available through 'MG-RAST'"
+        data_source = self.matcher.data_source(text)
+        self.assertEqual(data_source, "mg-rast")
+
+    def test_bioproject_as_url(self):
+        text = "All data available via https://www.ncbi.nlm.nih.gov/bioproject."
+        self.assertEqual(self.matcher.data_source(text), 'bioproject')
 
     def test_cannot_find_any_data_source(self):
         text = """

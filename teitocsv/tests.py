c1418fad2eed0a7e5bf03d302df878880c3f6d69
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-06-18 09:32:30 +0200

    Removal list expressions b/c it's already passed as list

--- teitocsv/tests.py
+++ teitocsv/tests.py
@@ -14,7 +14,7 @@ class TestAccessionNumberMatcher(unittest.TestCase):
         This will give us this.
         """
 
-        matches = list(self.matcher.accession_numbers(text))
+        matches = self.matcher.accession_numbers(text)
         self.assertEqual(['ERP123456'], matches)
 
     def test_studies_pattern_matches_twice(self):
@@ -22,7 +22,7 @@ class TestAccessionNumberMatcher(unittest.TestCase):
         This is a text with accession number ERP123456. 
         This will give us this. DRP654321.
         """
-        matches = list(self.matcher.accession_numbers(text))
+        matches = self.matcher.accession_numbers(text)
         self.assertEqual(['ERP123456', 'DRP654321'], matches)
 
     def test_studies_pattern_does_not_match(self):
@@ -30,21 +30,21 @@ class TestAccessionNumberMatcher(unittest.TestCase):
         This is a text with accession number foobar. 
         This will give us this.
         """
-        matches = list(self.matcher.accession_numbers(text))
+        matches = self.matcher.accession_numbers(text)
         self.assertFalse(matches)
 
     def test_biosamples_pattern_matches_once(self):
         text = """
         This is a text with accession number SAMEF1234567890123456 637783848.
         """
-        matches = list(self.matcher.accession_numbers(text))
+        matches = self.matcher.accession_numbers(text)
         self.assertEqual(matches, ['SAMEF1234567890123456'])
 
     def test_sample_pattern_matches_twice(self):
         text = """
         This is a text with accession numbers ERS123456 and ERS654321.
         """
-        matches = list(self.matcher.accession_numbers(text))
+        matches = self.matcher.accession_numbers(text)
         self.assertEqual(matches, ['ERS123456', 'ERS654321'])
 
     def test_experiments_pattern_matches_four_times(self):
@@ -52,7 +52,7 @@ class TestAccessionNumberMatcher(unittest.TestCase):
         This is a text with accession numbers ERX123456, DRX123456, SRX123456 and
         SRX654321.
         """
-        matches = list(self.matcher.accession_numbers(text))
+        matches = self.matcher.accession_numbers(text)
         self.assertEqual(matches, ['ERX123456', 'DRX123456', 'SRX123456', 'SRX654321'])
 
     def test_analysis_pattern_matches_three_times(self):
@@ -63,7 +63,7 @@ class TestAccessionNumberMatcher(unittest.TestCase):
 
         Accession number SRZ333222 is contained in  [Foobar, 2000].
         """
-        accession_numbers = list(self.matcher.accession_numbers(text))
+        accession_numbers = self.matcher.accession_numbers(text)
         self.assertEqual(accession_numbers, ['ERZ111333', 'DRZ666333', 'SRZ333222'])
 
     def test_chinese_accession_number_matches(self):
@@ -71,7 +71,7 @@ class TestAccessionNumberMatcher(unittest.TestCase):
         This paragraph has accession number PRJCA001121.
 
         """
-        accession_numbers = list(self.matcher.accession_numbers(text))
+        accession_numbers = self.matcher.accession_numbers(text)
         self.assertEqual(accession_numbers, ['PRJCA001121'])
 
 
@@ -115,7 +115,7 @@ class DataSourceMatcherTest(unittest.TestCase):
         text = "All data available via https://www.ncbi.nlm.nih.gov/bioproject."
         self.assertEqual(self.matcher.data_source(text), 'bioproject')
 
-    def test_cannot_find_any_data_source(self):
+    def test_no_data_source(self):
         text = """
         Foo bar is great, but this is bogus.
         """
